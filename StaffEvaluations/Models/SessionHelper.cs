23583ae1d254dc87db9c82cf148f484da0ee8350
Nelson, Michael C <mikesweb@illinois.edu>
2017-09-11 11:07:12 -0500

    sesson timeout warning

--- StaffEvaluations/Models/SessionHelper.cs
+++ StaffEvaluations/Models/SessionHelper.cs
@@ -5,16 +5,20 @@ using System.Web;
 using System.Configuration;
 using System.Web.Mvc;
 
+
 namespace Mayur.Web.Attributes
 {
     public class SessionTimeoutAttribute : ActionFilterAttribute
     {
+        public TempDataDictionary TempData { get; set; }
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             HttpContext ctx = HttpContext.Current;
             if (HttpContext.Current.Session["Masquerade"] == null)
             {
                 filterContext.Result = new RedirectResult("~/Home/Index");
+                TempData["warning"] = "Your Masquerade session has timed out.";
                 return;
             }
             base.OnActionExecuting(filterContext);
